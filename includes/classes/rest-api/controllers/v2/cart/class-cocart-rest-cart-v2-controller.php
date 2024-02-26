00d84ca985d79d9f078cf578a4b4c209a4c6b636
Sébastien Dumont <mailme@sebastiendumont.com>
2024-02-26 19:14:45 +0100

    Improvement for variation validation to ensure the variation always has full data.

--- includes/classes/rest-api/controllers/v2/cart/class-cocart-rest-cart-v2-controller.php
+++ includes/classes/rest-api/controllers/v2/cart/class-cocart-rest-cart-v2-controller.php
@@ -638,6 +638,11 @@ class CoCart_REST_Cart_v2_Controller extends CoCart_API_Controller {
 					throw new DataException( 'cocart_invalid_variation_data', $message, 400 );
 				}
 
+				// Fills variation array with unspecified attributes that have default values. This ensures the variation always has full data.
+				if ( '' !== $expected_value && ! isset( $variation[ wc_variation_attribute_name( $attribute['name'] ) ] ) ) {
+					$variation[ wc_variation_attribute_name( $attribute['name'] ) ] = $expected_value;
+				}
+
 				// If no attribute was posted, only error if the variation has an 'any' attribute which requires a value.
 				if ( '' === $expected_value ) {
 					$missing_attributes[] = $attribute_label;
